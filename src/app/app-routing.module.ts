118c5cee1f764082e075447ceafddc4d2eacb79c
SEKHOBANA <nessarefilwe@gmail.com>
2020-02-14 11:29:51 +0200

    boss

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from './landing/landing.component';
+import { ServicesComponent } from './services/services.component';
+import {AboutComponent } from './about/about.component';
+import {ProjectsComponent } from './projects/projects.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {PartnersComponent} from './partners/partners.component';
+
+const routes: Routes = [
+  {path:'landing', component: LandingComponent},
+  {path:'about',component:AboutComponent},
+  {path:'services',component: ServicesComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'partners',component:PartnersComponent},
+  {path:'',redirectTo:'landing', pathMatch:'full'}
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ServicesComponent } from './services/services.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PartnersComponent } from './partners/partners.component';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    ServicesComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    PartnersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
