dccec29dfb3a729b276d7521748c3e8dce564c09
Nomawethu <lethukuthulantokozo@gmail.com>
2020-02-18 13:46:29 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,8 @@ import {AboutComponent } from './about/about.component';
 import {ProjectsComponent } from './projects/projects.component';
 import {ContactsComponent} from './contacts/contacts.component';
 import {PartnersComponent} from './partners/partners.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-=======
 import { RegisterComponent } from './register/register.component';
->>>>>>> 42d029ba0c09eda7001f41f31e6610b0f18640d8
 
 const routes: Routes = [
   {path:'landing', component: LandingComponent},
@@ -19,11 +16,8 @@ const routes: Routes = [
   {path:'projects',component:ProjectsComponent},
   {path:'contacts',component:ContactsComponent},
   {path:'partners',component:PartnersComponent},
-<<<<<<< HEAD
   {path:'login',component:LoginComponent},
-=======
   {path:'register', component: RegisterComponent },
->>>>>>> 42d029ba0c09eda7001f41f31e6610b0f18640d8
   {path:'',redirectTo:'landing', pathMatch:'full'}
 
 ];
