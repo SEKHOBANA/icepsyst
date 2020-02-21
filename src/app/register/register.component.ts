47c1de68e35ec738364cc12f7e77d535eff2b959
Moora <43159712+MooraBlack@users.noreply.github.com>
2020-02-21 10:39:14 +0200

    register
    additional

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from './../users.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  // @Input() applyData = {
+  //   fname: " ", lname: " ", email: " ", pword: " ", pword2: " "
+  // } 
   constructor() { }
-
-  ngOnInit(): void {
+  // private users_Service: UsersService
+  ngOnInit() {
+  
   }
+
+  // postApplication()
+  // {
+  //   this.users_Service.PostApplication(this.applyData).subscribe(
+  //     data => console.log(data));
+  //     console.log(this.applyData.fname);
+  //     console.log(this.applyData.lname);
+  //     console.log(this.applyData.email);
+  //     console.log(this.applyData.pword);
+  //     console.log(this.applyData.pword2);
+  // }
 
 }
